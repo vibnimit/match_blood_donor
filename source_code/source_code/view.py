f0daca80ed1aad0b2e1ed3d2deea7bb9759b2db9
Vibhu Varshney <varshney.vibhu72@gmail.com>
2019-09-21 23:51:20 -0700

    GeoJson working

--- source_code/source_code/view.py
+++ source_code/source_code/view.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from .models import BloodDonatingUsers
 import csv
-import pickle
 from django.core import serializers
+import json
 
 def test_method(request):
     # print(request.__dict__)
@@ -10,10 +10,34 @@ def test_method(request):
         bloodGroup = request.GET.get('blood_group')
         donating_users_list = BloodDonatingUsers.objects.filter(blood_group=bloodGroup, label=1)
         print(len(donating_users_list))
+
+
         du_json = serializers.serialize('json', donating_users_list)
-        return HttpResponse(du_json, content_type='application/json')
+        print(du_json)
+        du_geoJson = createGeoJson(du_json)
+        print(du_geoJson)
+        return HttpResponse(json.dumps(du_geoJson), content_type='application/json')
     return HttpResponse("Text only, please.", content_type="text/plain")
 
+def createGeoJson(records):
+    dict = {"features":[],"metadata":[]}
+    records_json = json.loads(records)
+    for row in records_json:
+        prop = {
+            "Name":row['fields']['name'],
+            "BloodGroup": row['fields']['blood_group'],
+            "Phone": row['fields']['phone']
+        }
+
+        geometry = {
+            "type":"Point",
+            "coordinates":[row['fields']['longitude'], row['fields']['latitude']]
+        }
+
+        rec_dicts = {"properties":prop,"geometry":geometry}
+
+        dict["features"].append(rec_dicts)
+    return dict
 
 def load_data_from_csv():
     with open("source_code/table_data.csv") as f:
